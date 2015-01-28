06dfb067a97f603e411ffb17b7027193cf772c4c
Patrick Merlot <patrick.merlot@gmail.com>
2015-01-28 14:11:17 +0100

    deliberately breaking the code to see it on Wercker

--- app_test.py
+++ app_test.py
@@ -6,7 +6,7 @@ import json
 class CitiesTestCase(unittest.TestCase):
 
   def test_index(self):
-    tester = app.test_client(self)
+      tester = app.test_client(self)
     response = tester.get('/cities.json', content_type='application/json')
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.data, json.dumps(['Amsterdam', 'San Francisco', 'Berlin', 'New York']))
